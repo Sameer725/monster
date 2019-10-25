10df98ae83cfd9988c4273630a775731e60f5df0
Sam Eer <samkhanal725@gmail.com>
2019-10-25 18:15:43 +0545

    Updates

---
+++ precache-manifest.00b9e8beeb84c23a9e9fb5eb5198a615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b233e25b582744aa9d55aa17129a25df",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "9f274e54bd7804b1746d",
+    "url": "/monster/static/css/main.c48247cb.chunk.css"
+  },
+  {
+    "revision": "3f85d9169e1ecf70ea00",
+    "url": "/monster/static/js/2.1768bef8.chunk.js"
+  },
+  {
+    "revision": "9f274e54bd7804b1746d",
+    "url": "/monster/static/js/main.0059057c.chunk.js"
+  },
+  {
+    "revision": "ca46c9863f132609add7",
+    "url": "/monster/static/js/runtime-main.043d9656.js"
+  }
+]);
